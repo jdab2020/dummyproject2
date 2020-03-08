e8f44147637d13c2f79880d53310ce941936ff0c
jdab2020 <jdabalos90@gmail.com>
2020-03-08 00:50:10 -0800

    targeting and fetching problems

--- routes/api-routes-landing.js
+++ routes/api-routes-landing.js
@@ -9,30 +9,31 @@ module.exports = function (app) {
                 search = { symbols: result };
                 // loop query
                 console.log(search, "get from database ==================");
+                console.log(result, "result ===========")
                 console.log(search.symbols.length, "search.symbols.length =====================")
                 console.log(search.symbols.Stock, "search.symbols.stock ===============")
                 let queriesNews = [];
                 let queriesStocks = [];
-
-                Object.keys(search).forEach (element => {
-                    console.log(this.dataValues, "this.dataValues.symbol ===========")
-                    let queryURL_news = "https://stocknewsapi.com/api/v1?tickers=" + this.dataValues + "&items=10&token=" + process.env.apiKeyStockNews;
+                console.log(result[0].Stock, "=================")
+                for (let Stock in result) (element => {
+                    console.log(this,"=========================")
+                    let queryURL_news = "https://stocknewsapi.com/api/v1?tickers=" + this.dataValues.symbol + "&items=10&token=" + process.env.apiKeyStockNews;
                     fetch(queryURL_news)
                         .then((response) => {
                             console.log(response, "response news ===================");
                             queriesNews.push(response.json());
                         });
-                    let queryURL_stocks = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + this.dataValues + "&apikey=" + process.env.apiKeyAlphaVantage1;
+                    let queryURL_stocks = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + this.dataValues.symbol + "&apikey=" + process.env.apiKeyAlphaVantage1;
                     fetch(queryURL_stocks)
                         .then((response) => {
                             console.log(response, "response stocks ====================");
                             queriesStocks.push(response.json());
                         })
                 })
+                console.log(queriesStocks,"queries stocks api ============");
+                console.log(queriesNews,"queries news api ==============")
 
-
-
-                return Promise.all(queriesNews, queriesStocks);
+                // return Promise.all(queriesNews, queriesStocks);
             }).then(function (results) {
                 console.log(results, "results =============")
                 let search = { search: results }
