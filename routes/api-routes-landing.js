c30c606062ee54dea3e35043183eb62ab070e285
jdab2020 <jdabalos90@gmail.com>
2020-03-08 10:36:55 -0700

    added fetch | debugging results

--- routes/api-routes-landing.js
+++ routes/api-routes-landing.js
@@ -1,4 +1,6 @@
 const db = require("../models");
+const fetch = require('node-fetch');
+
 module.exports = function (app) {
     let search;
 
@@ -10,37 +12,42 @@ module.exports = function (app) {
                 // loop query
                 // console.log(search, "get from database ==================");
                 // console.log(result, "result ===========");
-                console.log(search.symbols.length, "search.symbols.length =====================");
-                console.log(result.length, "result.length ===============");
+                // console.log(search.symbols.length, "search.symbols.length =====================");
+                // console.log(result.length, "result.length ===============");
                 let queriesNews = [];
                 let queriesStocks = [];
-                console.log(result[0], "result[0]=================");
-                console.log(result[0].dataValues.symbol,"result[0].dataValues.symbol==============")
+                // console.log(result[0], "result[0]=================");
+                // console.log(result[0].dataValues.symbol,"result[0].dataValues.symbol==============")
 
                 for (let i = 0; i < result.length; i++ ) {
                     console.log("hello =========================")
-                    let queryURL_news = "https://stocknewsapi.com/api/v1?tickers=" + result[i].dataValues.symbol + "&items=10&token=" + process.env.apiKeyStockNews;
-                    fetch(queryURL_news)
-                        .then((response) => {
-                            console.log(response, "response news ===================");
-                            queriesNews.push(response.json());
-                        });
-                    let queryURL_stocks = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + result[i].dataValues.symbol + "&apikey=" + process.env.apiKeyAlphaVantage1;
-                    fetch(queryURL_stocks)
-                        .then((response) => {
-                            console.log(response, "response stocks ====================");
-                            queriesStocks.push(response.json());
-                        })
+                    let queryURL_news = "https://stocknewsapi.com/api/v1?tickers=" + result[i].dataValues.symbol.toUpperCase() + "&items=10&token=" + process.env.apiKeyStockNews;
+                    queriesNews.push(fetch(queryURL_news).then(results=>{results.json()}))
+                        // .then((response) => {
+                        //     console.log(response, "response news ===================");
+                        //     queriesNews.push(response.json());
+                        // }).catch((err)=>{if (err) throw err})
+                    // let queryURL_stocks = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + result[i].dataValues.symbol.toUpperCase() + "&apikey=" + process.env.apiKeyAlphaVantage1;
+                    // queriesStocks.push(fetch(queryURL_stocks))
+                        // .then((response) => {
+                        //     console.log(response, "response stocks ====================");
+                        //     queriesStocks.push(response.json());
+                        // }).catch((err)=>{if (err) throw err})
                 }
-                console.log(queriesStocks,"queries stocks api ============");
-                console.log(queriesNews,"queries news api ==============")
+                // console.log(queriesStocks,"queries stocks api ============");
+                // console.log(queriesNews,"queries news api ==============")
 
                 // return Promise.all(queriesNews, queriesStocks);
-            }).then(function (results) {
-                console.log(results, "results =============")
-                let search = { search: results }
-                console.log(search, "search ==================")
-                res.render("index", search)
+                console.log(queriesNews,"queriesNews=========")
+                return Promise.all(queriesNews)
+                
+
+            }).then( (results) => {
+                // console.log(results, "results =============");
+                console.log(results, "results==============");
+                // let search = { search: results }
+                // console.log(queriesStocks, "queriesStocks ==================")
+                // res.render("index", search)
             });
     });
     // posting into database works
