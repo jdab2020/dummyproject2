5b6a8b79d061338345c5c02afeeb3ce6094c43cc
jdab2020 <jdabalos90@gmail.com>
2020-03-08 10:58:02 -0700

    debugging results

--- routes/api-routes-landing.js
+++ routes/api-routes-landing.js
@@ -19,36 +19,31 @@ module.exports = function (app) {
                 // console.log(result[0], "result[0]=================");
                 // console.log(result[0].dataValues.symbol,"result[0].dataValues.symbol==============")
 
-                for (let i = 0; i < result.length; i++ ) {
+                for (let i = 0; i < result.length; i++) {
                     console.log("hello =========================")
                     let queryURL_news = "https://stocknewsapi.com/api/v1?tickers=" + result[i].dataValues.symbol.toUpperCase() + "&items=10&token=" + process.env.apiKeyStockNews;
-                    queriesNews.push(fetch(queryURL_news).then(results=>{results.json()}))
-                        // .then((response) => {
-                        //     console.log(response, "response news ===================");
-                        //     queriesNews.push(response.json());
-                        // }).catch((err)=>{if (err) throw err})
+                    queriesNews.push(fetch(queryURL_news).then(results => { results.json() }))
+                    // .then((response) => {
+                    //     console.log(response, "response news ===================");
+                    //     queriesNews.push(response.json());
+                    // }).catch((err)=>{if (err) throw err})
                     // let queryURL_stocks = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + result[i].dataValues.symbol.toUpperCase() + "&apikey=" + process.env.apiKeyAlphaVantage1;
                     // queriesStocks.push(fetch(queryURL_stocks))
-                        // .then((response) => {
-                        //     console.log(response, "response stocks ====================");
-                        //     queriesStocks.push(response.json());
-                        // }).catch((err)=>{if (err) throw err})
+                    // .then((response) => {
+                    //     console.log(response, "response stocks ====================");
+                    //     queriesStocks.push(response.json());
+                    // }).catch((err)=>{if (err) throw err})
                 }
                 // console.log(queriesStocks,"queries stocks api ============");
                 // console.log(queriesNews,"queries news api ==============")
 
                 // return Promise.all(queriesNews, queriesStocks);
-                console.log(queriesNews,"queriesNews=========")
+                // console.log(queriesNews,"queriesNews=========")
                 return Promise.all(queriesNews)
-                
 
-            }).then( (results) => {
-                // console.log(results, "results =============");
-                console.log(results, "results==============");
-                // let search = { search: results }
-                // console.log(queriesStocks, "queriesStocks ==================")
-                // res.render("index", search)
-            });
+
+            }).then(results => { console.log(results, "results=================") 
+            }).catch((err) => { if (err) throw err });
     });
     // posting into database works
     app.post("/api/symbols", function (req, res) {
